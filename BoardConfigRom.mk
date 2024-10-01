990a2093247e937d5276afe25dea657df7f9e714
Libra420T <ryantf420@gmail.com>
2024-10-01 12:13:27 -0400

    Fetch for Matrix File

--- BoardConfigRom.mk
+++ BoardConfigRom.mk
@@ -1,2 +1,10 @@
 # Build Broken ELF
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# Manifests
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/rom/config/device_framework_matrix.xml
+DEVICE_MANIFEST_FILE += \
+    device/google/zuma/android.hardware.security.rkp-service.citadel.xml \
+    device/google/zuma/manifest_radio_ds.xml
+    
+
