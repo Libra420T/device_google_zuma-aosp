b53c77aed351d7ce28aea236b9e10b86dee382a1
Libra420T <ryantf420@gmail.com>
2024-10-01 12:16:03 -0400

    Fetch for Partion & Chain Partition

--- BoardConfigRom.mk
+++ BoardConfigRom.mk
@@ -7,4 +7,21 @@ DEVICE_MANIFEST_FILE += \
     device/google/zuma/android.hardware.security.rkp-service.citadel.xml \
     device/google/zuma/manifest_radio_ds.xml
     
+# Partitions
+AB_OTA_PARTITIONS += \
+    vendor \
+    vendor_dlkm
 
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
+
+# Enable chain partition for vendor.
+BOARD_AVB_VBMETA_VENDOR := vendor
+BOARD_AVB_VBMETA_VENDOR_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_VBMETA_VENDOR_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
+
+AB_OTA_PARTITIONS += \
+    vbmeta_vendor
