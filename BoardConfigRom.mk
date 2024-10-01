8569bb5c7fe5618e4bedb5f38f5669a7f1879e97
Libra420T <ryantf420@gmail.com>
2024-10-01 12:25:45 -0400

    Use a more Modern Arch & cpu

--- BoardConfigRom.mk
+++ BoardConfigRom.mk
@@ -1,5 +1,7 @@
 # Build Broken ELF
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+# Build Broken Duplicates
+BUILD_BROKEN_DUP_RULES := true
 
 # Manifests
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/rom/config/device_framework_matrix.xml
